6e29b104ff0f844a98fb15cc53bfa7edb8521d20
dyahzhafira <dyahzhafira.w@gmail.com>
2026-09-18 10:12:07 +0000

    feat: add sysinfo.sh info

--- sysinfo.sh
+++ sysinfo.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+get_os_info(){
+    local pretty_name kernel
+    pretty_name=$(grep '^PRETTY_NAME=' /etc/os-release | cut -d= -f2 | tr -d '"')
+    kernel=$(uname -r)
+    echo "$pretty_name (Kernel $kernel)"
+}
+
+get_regular_users(){
+    awk -F: '$3 >= 1000 && $3 <6000 {count++} END {print count+0}' /etc/passwd
+}
+
+get_running_processes(){
+    ps -e --no-headers | wc -l
+}
+
+get_virtualization(){
+    local virt
+    virt=$(systemd-detect-virt 2>/dev/null)
+    case "$virt" in
+        oracle) echo "Terdeteksi (VirtualBox)" ;;
+        none|"") echo "Tidak terdeteksi (kemungkinan mesin fisik)" ;;
+        *) echo "Terdeteksi ($virt)";; 
+    esac
+}
+
+echo "OS/Kernel: $(get_os_info)"
+echo "Akun pengguna: $(get_regular_users) akun"
+echo "Prose berjalan: $(get_running_processes) proses"
+echo "Virtualisasi: $(get_virtualization)"
+
 # Metric 1: Memory Usage
 total=$(free -m | awk '/Mem:/ {print $2}')
 available=$(free -m | awk '/Mem:/ {print $7}')
